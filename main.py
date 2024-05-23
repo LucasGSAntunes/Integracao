4658a9fc34d4acdeb87a5396d9d7c38d9be37774
LucasGSAntunes <190091681@aluno.unb.br>
2024-05-23 19:10:51 -0300

    bugfix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from models.LeadModel import Lead
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -11,3 +12,7 @@ async def root():
 @app.post("/data/")
 async def get_data(dados: Lead):
     return dados
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
