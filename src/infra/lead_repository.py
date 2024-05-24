90de5dec597a3cfd975976570ad654aaa38c3048
LucasGSAntunes <190091681@aluno.unb.br>
2024-05-24 18:23:38 -0300

    bugfix

--- src/infra/lead_repository.py
+++ src/infra/lead_repository.py
@@ -6,7 +6,7 @@ from src.domain.interfaces.abstract_repositories import ABCLeadRepository
 
 class LeadRepository(ABCLeadRepository):
     def create(self, lead: Lead) -> Dict:
-        url = "https://api.exactspotter.com/v3/Leads"
+        url = "https://api.exactspotter.com/v3/LeadsAdd"
         token = "d53a044d-913a-4e82-bf85-9bb30be1d74f"
 
         headers = {
