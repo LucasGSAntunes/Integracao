c9ec7ced5dd6fb4dc1aa438cf013ee0191da5861
LucasGSAntunes <190091681@aluno.unb.br>
2024-05-24 14:47:00 -0300

    bugfix

--- src/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-from pydantic_settings import BaseSettings, SettingsConfigDict 
-import os
-
-load_dotenv()
-
-class Settings(BaseSettings):
-    API_VERSION: str = "1.0.0"
-    
